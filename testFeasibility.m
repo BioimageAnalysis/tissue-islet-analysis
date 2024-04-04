834e9690e110da22077fb582d870170310ed6114
Jian Tay <jian.tay@colorado.edu>
2024-04-04 14:05:59 -0600

    Updated segmentation parameters

--- testFeasibility.m
+++ testFeasibility.m
@@ -1,15 +1,16 @@
 clearvars
 clc
 
-reader = BioformatsImage('D:\Documents\OneDrive - UCB-O365\Projects\2024 Hill\cdubvGF_101623\2023_10_16__0013.czi');
+%reader = BioformatsImage('D:\Documents\OneDrive - UCB-O365\Projects\2024 Hill\cdubvGF_101623\2023_10_16__0013.czi');
+reader = BioformatsImage('C:\Users\Jian Tay\OneDrive - UCB-O365\Projects\2024 Hill\cdubvGF_101623\2023_10_16__0013.czi');
 
-numTiles = [50, 50];
+numTiles = [25, 25];
 
 maskTissueFull = false(reader.height, reader.width);
-maskIsletFull = false(reader.height, reader.width);
+maskIsletsFull = false(reader.height, reader.width);
 
 tStart = tic;
-for ii = 1:prod(numTiles)
+for ii = 160%1:prod(numTiles)
 
     % Ired = getPlane(reader, 1, 1, 1, 'ROI', [10000, 10000, 1000, 1000]);
     % Igreen = getPlane(reader, 1, 2, 1, 'ROI', [10000, 10000, 1000, 1000]);
@@ -21,10 +22,12 @@ for ii = 1:prod(numTiles)
 
     Irgb = cat(3, Ired, Igreen, Iblue);
 
+    imshow(Irgb,[])
+
     %%
 
     %Target RGB color of islets as a 1x1x3 vector.
-    spotColorRGB = cat(3, 92, 46, 23);
+    spotColorRGB = cat(3, 92, 63, 33);
 
     %Convert the spot RGB color to L*a*b color (Note: The RGB color has to be
     %an unsigned 8-bit integer to match the original data).
@@ -41,13 +44,17 @@ for ii = 1:prod(numTiles)
     bb = labImg(:, :, 3);
 
     %Find similar colors in the image to the spot above
-    maskIslets = (aa - spotColorLab(2)).^2 + (bb - spotColorLab(3)).^2 <= 20^2;
-    maskIslets = imopen(maskIslets, strel('disk', 3));
+    maskIslets = (aa - spotColorLab(2)).^2 + (bb - spotColorLab(3)).^2 <= 10^2;
+    maskIslets = bwareaopen(maskIslets, 5);
+   
+    %imshow(maskIslets)
 
     % maskTissue = (aa - tissueColorLab(2)).^2 + (bb - tissueColorLab(3)).^2 <= 20^2;
     % maskTissue = imopen(maskTissue, strel('disk', 3));
-    maskTissue = Igreen < 180 & Igreen > 5;
-    maskTissue = imclose(maskTissue, strel('disk', 10));
+
+    maskTissue = Igreen < 190 & Igreen > 5;
+    %maskTissue = imclose(maskTissue, strel('disk', 10));
+    imshowpair(Irgb, maskTissue)
 
     maskTissueFull(rect(2):(rect(2)+rect(4) - 1), rect(1):(rect(1)+rect(3) - 1)) = maskTissue;
     maskIsletsFull(rect(2):(rect(2)+rect(4) - 1), rect(1):(rect(1)+rect(3) - 1)) = maskIslets;
@@ -62,5 +69,5 @@ for ii = 1:prod(numTiles)
 end
 toc(tStart)
 
-imwrite(maskIsletsFull, 'test_isletMask.tif', 'Compression', 'none');
-imwrite(maskTissueFull, 'test_tissueMask.tif', 'Compression', 'none');
+% imwrite(maskIsletsFull, 'test_isletMask.tif', 'Compression', 'none');
+% imwrite(maskTissueFull, 'test_tissueMask.tif', 'Compression', 'none');
